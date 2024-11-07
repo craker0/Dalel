5cbf29ea07e18fad9dcdfa3d4f8c8e1de6c38834
craker0 <behmailk40@gmail.com>
2024-11-07 15:36:15 +0200

    first commit

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imgForgetpass
+  /// assets/img/forgetpass.png
+  static const String imgForgetpass = "assets/img/forgetpass.png";
+
+  /// Assets for imgOnBoarding1
+  /// assets/img/onBoarding1.png
+  static const String imgOnBoarding1 = "assets/img/onBoarding1.png";
+
+  /// Assets for imgOnBoarding2
+  /// assets/img/onBoarding2.png
+  static const String imgOnBoarding2 = "assets/img/onBoarding2.png";
+
+  /// Assets for imgOnBoarding3
+  /// assets/img/onBoarding3.png
+  static const String imgOnBoarding3 = "assets/img/onBoarding3.png";
+
+  /// Assets for imgSuccess
+  /// assets/img/Success.png
+  static const String imgSuccess = "assets/img/Success.png";
+
+  /// Assets for imgVector1
+  /// assets/img/Vector1.svg
+  static const String imgVector1 = "assets/img/Vector1.svg";
+
+  /// Assets for imgVector2
+  /// assets/img/Vector2.svg
+  static const String imgVector2 = "assets/img/Vector2.svg";
+}
+
